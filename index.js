2bc46dcc190b1bd7e1eacb6536e2b71322c80401
ajitdhanukmandal <theajitmandal@gmail.com>
2023-09-20 15:41:53 +0545

    this is my new commit

---
+++ index.js
@@ -0,0 +1,12 @@
+myNumber1 = [2,7,1,8,10,5,3];
+
+number2 = myNumber1.sort();
+number3 = myNumber1.reverse();
+
+console.log(number2);
+console.log(number3);
+
+// console.log(myNumber1.sort());
+// console.log(myNumber1.reverse());
+
+//sort cannot sort numbers
