20169786f8a55247f3a4106f6f9407c1b23be7fa
Ajit Mandal <theajitmandal@gmail.com>
2024-09-07 22:25:16 +0545

    just practice

--- tt/#012_Maths_Object.js
+++ tt/#012_Maths_Object.js
@@ -45,6 +45,8 @@ console.log(Math.floor(Math.random()*10)); //between 0 to 9
 /* Math.trunc() -> returns the integer part of a number) */
 console.log(Math.trunc(4.6));
 console.log(Math.trunc(-99.2));
+console.log(Math.trunc(89.0));
+console.log(Math.trunc(898.002));
 
 // Practice Time : If the argument is a +ve number, Math.trunc() is equivalent to Math.floor otherwise Math.trunc() is 
 // equivalent to Math.ceil()
