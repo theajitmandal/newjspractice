e76b58168577c33f5bd16620967c90f16a49499b
Ajit Mandal <theajitmandal@gmail.com>
2024-11-23 19:32:27 +0545

    just practice

--- practice.js
+++ practice.js
@@ -285,6 +285,7 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const nums = [2, 4, 6, 8, 10, 12];
+const numsNumsNumss = [2, 4, 6, 8, 10, 12, 14, 15];
 
 const newNums = myNums.filter( (item) => item > 4  );
 console.log(newNums);
