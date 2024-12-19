4d049da4319950390284d0ba955ab44e09695e0a
Ajit Mandal <theajitmandal@gmail.com>
2024-12-19 22:00:21 +0545

    just practice

---
+++ cac/#038_2Projects.js
@@ -0,0 +1,4 @@
+/*
+    Project No: 5 -> Unlimited Colors (In newprojects  folder)
+    Project No: 6 -> Keyboard Check (In newprojects  folder)
+*/
---
+++ cac/#039_API_Request_And_V8_Engine.js
@@ -0,0 +1,24 @@
+/*
+    randomuser.me
+    jsonformatter.org
+
+    https://api.github.com/users/theajitmandal
+
+    Learn XMLHttpRequest:
+        XMLHttpRequest (XHR) objects are used to interact with servers. 
+        You can retrieve data from a URL without having to do a full page refresh. 
+        This enables a Web page to update just part of a page without disrupting what the user is doing.
+
+        The XMLHttpRequest.readyState property returns the state an XMLHttpRequest client is in. 
+        An XHR client exists in one of the following states:
+           Value                       State                       Description
+            0	                UNSENT	                Client has been created. open() not called yet.
+            1	                OPENED	                open() has been called.
+            2	                HEADERS_RECEIVED	    send() has been called, and headers and status are available.
+            3	                LOADING	                Downloading; responseText holds partial data.
+            4	                DONE	                The operation is complete.
+
+
+        
+    github.com/v8   -> V8 Chrome Engine
+*/
