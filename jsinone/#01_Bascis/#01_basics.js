e868d1d60dd55fd5a58296be096fc5033e246f2b
Ajit Mandal <theajitmandal@gmail.com>
2025-01-06 07:48:44 +0545

    started js revision

---
+++ jsinone/#01_Bascis/#01_basics.js
@@ -0,0 +1,105 @@
+// Build confidence to perform tasks in programming
+// Build projects after completion of basics
+// Until and Unless you don't build projects, you'll not build confidence to crack interviews
+
+/*
+    *** Learning to code is not so easy, but there is nothing 'not so easy' for me. So Coding is easy, very easy ***
+
+    How to stay motivated while learning to code?
+        01 Set goals
+        02 Make a routine
+        03 Find a mentor
+        04 Learn to self-motivate
+        05 Be realistic
+        06 Enjoy the process
+        07 Take breaks
+        08 Seek support
+        09 Challenge yourself
+        10 Keep up with new technologies
+
+    *** The only way to become a clever programmer is to: Practice. Practice. Practice. Code. Code. Code ! ***
+                        Practice. Practice. Practice. Code. Code. Code !
+    *** The only way to become a clever programmer is to: Practice. Practice. Practice. Code. Code. Code ! ***
+*/
+
+/*
+    *** JS Introduction ***
+    
+    JavaScript is the world's most popular programming language.
+    JavaScript is the programming language of the Web.
+    JavaScript is easy to learn.
+
+    Summary Definition of JS:
+        JavaScript (JS) is the most popular lightweight, scripting, and interpreted programming language. 
+        It was developed by Brendan Eich in 1995. JavaScript is popular as a scripting language for web pages, 
+        mobile apps, web servers, and many other platforms. It is essential for both front-end and 
+        back-end developers. It is also called as browser's language. It is the third layer of the layer cake of 
+        standard web technologies, two of which (HTML and CSS). JS enables to:
+            1. create dynamically updating contents
+            2. control multimedia
+            3. animate images
+            4. display interactive maps
+            5. animate 2D/3D graphics
+        For Example,
+            JS can change the HTML Content
+            JS can change the HTML attribute values
+            JS can change the HTML Styles (CSS)
+            JS can hide/show the HTML elements
+            
+    JavaScript (JS) is the most popular lightweight, scripting, and interpreted programming language. 
+    It was developed by Brendan Eich in 1995. JavaScript is well-known as a scripting language for web pages, 
+    mobile apps, web servers, and many other platforms. It is essential for both front-end and back-end developers 
+    to have a strong command of JavaScript, as many job roles require fluency in this language. It is also called
+    browser's language.
+
+    JavaScript is a scripting or programming language that allows you to implement complex features on web pages — 
+    every time a web page does more than just sit there and display static information for you to look at — 
+    displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — 
+    you can bet that JavaScript is probably involved. It is the third layer of the layer cake of 
+    standard web technologies, two of which (HTML and CSS).
+
+    HTML is the markup language that we use to structure and give meaning to our web content, 
+    for example defining paragraphs, headings, and data tables, or embedding images and videos in the page.
+
+    CSS is a language of style rules that we use to apply styling to our HTML content, for example setting 
+    background colors and fonts, and laying out our content in multiple columns.
+
+    JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, 
+    animate images, and pretty much everything else. (Okay, not everything, but it is amazing what you can achieve 
+    with a few lines of JavaScript code.)
+
+    *** Why JS? ***
+        JavaScript is one of the 3 languages all web developers must learn:
+
+        1. HTML to define the content of web pages
+        2. CSS to specify the layout of web pages
+        3. JavaScript to program the behavior of web pages:
+            It improves the user experience of a web page by converting it from a static page into an interactive one
+            and adds behaviour to the web page.
+
+   Here we will learn every version of JavaScript:
+
+        The Original JavaScript ES1 ES2 ES3 (1997-1999)
+        The First Main Revision ES5 (2009)
+        The Second Revision ES6 (2015)
+        The Yearly Additions (2016, 2017 ... 2021, 2022)
+    
+    *** The only way to become a clever programmer is to: Practice. Practice. Practice. Code. Code. Code ! ***
+
+    Commonly Asked Questions / CAQ / FAQ:
+        How do I get JavaScript?
+        Where can I download JavaScript?
+        Is JavaScript Free?
+
+    You don't have to get or download JavaScript.
+    JavaScript is already running in your browser on your computer, on your tablet, and on your smart-phone.
+    JavaScript is free to use for everyone.
+
+    JavaScript References:
+        W3Schools maintains a complete JavaScript reference, including all HTML and browser objects.
+        The reference contains examples for all properties, methods and events, and is continuously updated 
+        according to the latest web standards.
+
+        MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/What_is_JavaScript
+*/
