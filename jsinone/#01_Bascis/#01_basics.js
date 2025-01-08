bcb3daba3e152ee386f48d136c0d3a08bd76cbe8
Ajit Mandal <theajitmandal@gmail.com>
2025-01-08 20:11:57 +0545

    learned about script tag

--- jsinone/#01_Bascis/#01_basics.js
+++ jsinone/#01_Bascis/#01_basics.js
@@ -45,6 +45,12 @@
             JS can change the HTML attribute values
             JS can change the HTML Styles (CSS)
             JS can hide/show the HTML elements
+
+    A scripting language is a type of programming language in which the instructions are interpreted 
+    individually at runtime.
+
+    Markup language is a system that uses tags or codes to define the structure and presentation of a 
+    document or web page.
             
     JavaScript (JS) is the most popular lightweight, scripting, and interpreted programming language. 
     It was developed by Brendan Eich in 1995. JavaScript is well-known as a scripting language for web pages, 
@@ -103,6 +109,13 @@
         MDN Web Docs:
             https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/What_is_JavaScript
 
+    *** To run JS codes ***
+        VS Code Download
+        Babel JS Download Extension
+        Make one folder and open it with code
+        To run JS 'index.js' file in VS code, type node index.js, for this node should be installed
+        Comment Shortcut ~ Ctrl + ?
+
     *** What can JS do? ***
         1. JavaScript Can Change HTML Content
         
@@ -143,7 +156,187 @@
             </body>
             </html>
 
+        3. JavaScript Can Change HTML Styles (CSS)
 
+            Changing the style of an HTML element, is a variant of changing an HTML attribute:
+
+            <!DOCTYPE html>
+            <html>
+            <body>
+                <h2>What Can JavaScript Do?</h2>
+                <p id="demo">JavaScript can change the style of an HTML element.</p>
+                <button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Click Me!</button>
+            </body>
+            </html> 
+
+        4. JavaScript Can Hide HTML Elements
+        
+            Hiding HTML elements can be done by changing the display style:
+
+            <!DOCTYPE html>
+            <html>
+            <body>
+                <h2>What Can JavaScript Do?</h2>
+                <p id="demo">JavaScript can hide HTML elements.</p>
+                <button type="button" onclick="document.getElementById('demo').style.display='none'">Click Me!</button>
+            </body>
+            </html> 
+
+        5. JavaScript Can Show HTML Elements
+        
+            Showing hidden HTML elements can also be done by changing the display style:
+
+            <!DOCTYPE html>
+            <html>
+            <body>
+                <h2>What Can JavaScript Do?</h2>
+                <p>JavaScript can show hidden HTML elements.</p>
+                <p id="demo" style="display:none">Hello JavaScript!</p>
+                <button type="button" onclick="document.getElementById('demo').style.display='block'">Click Me!</button>
+            </body>
+            </html> 
+
+    *** Did You Know? ***
+
+        JavaScript and Java are completely different languages, both in concept and design.
+        JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997.
+        ECMA-262 is the official name of the standard. ECMAScript is the official name of the language.
+
+    *** Where to inser JS codes ? ***
+        
+        The <script> Tag
+            In HTML, JavaScript code is inserted between <script> and </script> tags.
+
+            Example: 
+            <script>
+                document.getElementById("demo").innerHTML = "My First JavaScript";
+            </script>
+
+        Old JavaScript examples may use a type attribute: <script type="text/javascript">.
+        The type attribute is not required. JavaScript is the default scripting language in HTML.
+
+    *** JavaScript Functions and Events ***
+
+        A JavaScript function is a block of JavaScript code, that can be executed when "called" for.
+        For example, a function can be called when an event occurs, like when the user clicks a button.
+        We will learn much more about functions and events in later chapters.
+
+    *** JavaScript in <head> or <body> ***
+
+        You can place any number of scripts in an HTML document.
+        Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
+
+    *** JavaScript in <head> ***
+        
+        In this example, a JavaScript function is placed in the <head> section of an HTML page.
+        The function is invoked (called) when a button is clicked:
+
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <script>
+                function myFunction() {
+                    document.getElementById("demo").innerHTML = "Paragraph changed.";
+                }
+            </script>
+            </head>
+            <body>
+
+            <h2>Demo JavaScript in Head</h2>
+
+            <p id="demo">A Paragraph.</p>
+
+            <button type="button" onclick="myFunction()">Try it</button>
+
+            </body>
+            </html> 
+
+    *** JavaScript in <body> ***
+
+        In this example, a JavaScript function is placed in the <body> section of an HTML page.
+        The function is invoked (called) when a button is clicked:
+
+            <!DOCTYPE html>
+            <html>
+            <body>
+
+            <h2>Demo JavaScript in Body</h2>
+
+            <p id="demo">A Paragraph.</p>
+
+            <button type="button" onclick="myFunction()">Try it</button>
+
+            <script>
+            function myFunction() {
+                document.getElementById("demo").innerHTML = "Paragraph changed.";
+            }
+            </script>
+
+            </body>
+            </html> 
+
+    Note: Placing scripts at the bottom of the <body> element improves the display speed, 
+    because script interpretation slows down the display.
+
+    *** External JavaScript ***
+
+        Scripts can also be placed in external files:
+
+        External file: myScript.js
+        function myFunction() {
+            document.getElementById("demo").innerHTML = "Paragraph changed.";
+        }
+
+        External scripts are practical when the same code is used in many different web pages.
+        JavaScript files have the file extension .js.
+        To use an external script, put the name of the script file in the src (source) attribute of a <script> tag:
+
+        <script src="myScript.js"></script>
+
+        You can place an external script reference in <head> or <body> as you like.
+        The script will behave as if it was located exactly where the <script> tag is located.
+        External scripts cannot contain <script> tags.
+
+    *** External JavaScript Advantages ***
+
+        Placing scripts in external files has some advantages:
+
+        It separates HTML and code
+        It makes HTML and JavaScript easier to read and maintain
+        Cached JavaScript files can speed up page loads
+
+        To add several script files to one page  - use several script tags:
+        
+        <script src="myScript1.js"></script>
+        <script src="myScript2.js"></script>
+
+    *** External References ***
+        An external script can be referenced in 3 different ways:
+
+            With a full URL (a full web address)
+            With a file path (like /js/)
+            Without any path
+
+        This example uses a full URL to link to myScript.js:
+            <script src="https://www.w3schools.com/js/myScript.js"></script>
+
+        This example uses a file path to link to myScript.js:
+            <script src="/js/myScript.js"></script>
+
+        This example uses no path to link to myScript.js:
+            <script src="myScript.js"></script>
+
+        We can read more about file paths in the chapter HTML File Paths.
+
+
+
+
+
+
+
+
+
+        
 
 
 */
