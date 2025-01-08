782e09772f6572b5d59e014420c4ba50afc6a3df
Ajit Mandal <theajitmandal@gmail.com>
2025-01-08 08:30:05 +0545

    learning about what js can do

--- jsinone/#01_Bascis/#01_basics.js
+++ jsinone/#01_Bascis/#01_basics.js
@@ -102,4 +102,48 @@
 
         MDN Web Docs:
             https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/What_is_JavaScript
+
+    *** What can JS do? ***
+        1. JavaScript Can Change HTML Content
+        
+            One of many JavaScript HTML methods is getElementById().
+            The example below "finds" an HTML element (with id="demo"), 
+            and changes the element content (innerHTML) to "Hello JavaScript":
+
+            Example:
+
+            <!DOCTYPE html>
+            <html>
+            <body>
+                <h2>What Can JavaScript Do?</h2>
+                <p id="demo">JavaScript can change HTML content.</p>
+                <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>
+                    Click Me!
+                </button>
+            </body>
+            </html>
+
+            JavaScript accepts both double and single quotes:
+
+            document.getElementById('demo').innerHTML = 'Hello JavaScript';
+
+        2. JavaScript Can Change HTML Attribute Values
+            
+            In this example JavaScript changes the value of the src (source) attribute of an <img> tag:
+
+            <!DOCTYPE html>
+            <html>
+            <body>
+                <h2>What Can JavaScript Do?</h2>
+                <p>JavaScript can change HTML attribute values.</p>
+                <p>In this case JavaScript changes the value of the src (source) attribute of an image.</p>
+                <button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+                <img id="myImage" src="pic_bulboff.gif" style="width:100px">
+                <button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
+            </body>
+            </html>
+
+
+
+
 */
